1b191a69d3348990547cbb842b82a2aee4131596
Rafał <rafalgulewski@gmail.com>
2021-07-19 14:20:06 +0200

    changed navbar component

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -5,10 +5,20 @@ import '../styles/icons.css'
 
 import { Logo, Menu, NeonTextON } from './'
 import {Trans, Link} from 'gatsby-plugin-react-i18next';
+//const navScript = require('../scripts/navbar.js');
 
-const Navbar = (props) => {
-        
-        const changeMenuVisibility = () => {
+class Navbar extends React.Component {
+        state = {
+          currentPage: "",
+          prevScrollpos: 0
+        };
+    
+        constructor(props){
+          super(props);
+          this.state.currentPage = props.s;
+        }
+
+        changeMenuVisibility(){
                 const t = document.getElementsByClassName('nav-sl');
                 if(t[0].style.display === '')
                 {
@@ -16,36 +26,46 @@ const Navbar = (props) => {
                 }else{
                         for(let i=0, l=t.length; i<l; i++) t[i].style.display = '';
                 }
-        }
-        
-        const script = require('../scripts/navbar.js');
+          }
 
-        return (
-        <>
-                <script>script</script>
+        componentDidMount() {
+                const navScript = () => {
+                        var currentScrollPos = window.pageYOffset;
+                        if (this.state.prevScrollpos < 200 || this.state.prevScrollpos > currentScrollPos) {
+                                document.getElementById("navbar").style.top = "0";
+                        } else {
+                                document.getElementById("navbar").style.top = "-400px";
+                        }
+                                this.state.prevScrollpos = currentScrollPos;
+                        console.log(currentScrollPos);
+                }
+                document.addEventListener('scroll', navScript);
+        }
+    
+        render() {
+          return (
                 <div role="navigation" className="navMain" id="navbar">
-                        <div className='nav'>
-                                <Link to="/">
-                                <div className='nav-s nav-logo'>
-                                        <div className="logoStyle">
-                                                <Logo />
-                                        </div>
+                <div className='nav'>
+                        <Link to="/">
+                        <div className='nav-s nav-logo'>
+                                <div className="logoStyle">
+                                        <Logo />
                                 </div>
-                                </Link>
-                                <div className='s' />
-                                <div className='nav-button' role="button" tabIndex="0" onKeyPress={changeMenuVisibility} onClick={changeMenuVisibility}>
-                                        <Menu />
-                                </div>
-                                <NavbarLink a={props.s === "Umiejętności"} c={<Trans>skills</Trans>} nav="/skills" />
-                                <NavbarLink a={props.s === "Projekty"} c={<Trans>proj</Trans>} nav="/projects" />
-                                <NavbarLink a={props.s === "O mnie"} c={<Trans>about</Trans>} nav="/about" />
-                                <NavbarLink a={props.s === "Kontakt"} c={<Trans>contact</Trans>} nav="/contact" />
+                        </div>
+                        </Link>
+                        <div className='s' />
+                        <div className='nav-button' role="button" tabIndex="0" onKeyPress={this.changeMenuVisibility} onClick={this.changeMenuVisibility}>
+                                <Menu />
+                        </div>
+                        <NavbarLink a={this.state.currentPage === "Umiejętności"} c={<Trans>skills</Trans>} nav="/skills" />
+                        <NavbarLink a={this.state.currentPage === "Projekty"} c={<Trans>proj</Trans>} nav="/projects" />
+                        <NavbarLink a={this.state.currentPage === "O mnie"} c={<Trans>about</Trans>} nav="/about" />
+                        <NavbarLink a={this.state.currentPage === "Kontakt"} c={<Trans>contact</Trans>} nav="/contact" />
                         </div>
                 </div>
-        </>
-        )
+          );
+        }
 }
-
 
 const NavbarLink = (props) => {
         
